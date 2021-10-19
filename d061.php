abfb899f6c90a6254c7f788c048bb7c7aa9f07a5
Kotarooooo <wtrlndhr@gmail.com>
2021-10-19 15:52:26 +0900

    かっこをなくし、インデントを揃えて修正しました。

---
+++ d061.php
@@ -0,0 +1,11 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $chokolate = trim(fgets(STDIN));
+    $chokolates = calculateChokolate($chokolate);
+      echo $chokolates;
+    function calculateChokolate($chokolate){
+        if ($chokolate < 1) return 1;
+        return $chokolate * 3; 
+    }
+?>
