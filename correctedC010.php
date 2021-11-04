ea3f50f04da3aef4c5442efaade73ab4feceed68
Kotarooooo <wtrlndhr@gmail.com>
2021-11-04 13:03:43 +0900

    大宮さんからの課題20211104

---
+++ correctedC010.php
@@ -0,0 +1,16 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    [$xCoordinate, $yCoordinate, $noise] = explode(" ", trim(fgets(STDIN)));
+    $treeNum = trim(fgets(STDIN));
+    $twiceNoise = $noise **2;
+    for ($i = 0; $i < $treeNum; $i++) {
+        [$treeXCooodinate, $treeYCoodinate] = explode(" ", trim(fgets(STDIN)));
+        $conditionalExpression = ($treeXCooodinate - $xCoordinate) **2 + ($treeYCoodinate - $yCoordinate) **2;
+        if ($twiceNoise <= $conditionalExpression) {
+            echo "silent" ."\n";
+        } else {
+            echo "noisy" ."\n";
+        }
+    }
+?>
