38e8da2bab9d94f8ba776abeed2b27308aef6125
Kotarooooo <wtrlndhr@gmail.com>
2021-11-05 15:38:18 +0900

    まだ途中です。

---
+++ co32.php
@@ -0,0 +1,14 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $totalMerchandise = trim(fgets(STDIN));
+    for($i = 0; $i < $totalMerchandise; $i++) {
+        $dates = explode(" ", trim(fgets(STDIN)));
+        if ($dates == 0) {
+            $foodPrice = array_sum($date[1]);
+        }
+     
+    }
+            
+    
+?>
