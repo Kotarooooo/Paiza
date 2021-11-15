bf65b26956fd4d2d39fa8c25c393f449972eaed2
Kotarooooo <wtrlndhr@gmail.com>
2021-11-16 04:11:54 +0900

    6行目、11行目、13行目の訂正が完了しました。

--- correctedC010.php
+++ correctedC010.php
@@ -3,14 +3,14 @@
     // Let's チャレンジ！！
     [$xCoordinate, $yCoordinate, $noise] = explode(" ", trim(fgets(STDIN)));
     $treeNum = trim(fgets(STDIN));
-    $twiceNoise = $noise **2;
+    $twiceNoise = $noise ** 2;
     for ($i = 0; $i < $treeNum; $i++) {
         [$treeXCooodinate, $treeYCoodinate] = explode(" ", trim(fgets(STDIN)));
-        $conditionalExpression = ($treeXCooodinate - $xCoordinate) **2 + ($treeYCoodinate - $yCoordinate) **2;
+        $conditionalExpression = ($treeXCooodinate - $xCoordinate) ** 2 + ($treeYCoodinate - $yCoordinate) **2;
         if ($twiceNoise <= $conditionalExpression) {
-            echo "silent" ."\n";
+            echo "silent" . "\n";
         } else {
-            echo "noisy" ."\n";
+            echo "noisy" . "\n";
         }
     }
 ?>
