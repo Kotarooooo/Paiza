2096d2d939aebaa968665df0965f5d6acaaa693c
Kotarooooo <wtrlndhr@gmail.com>
2021-11-16 06:19:47 +0900

    6行目の変数名を訂正、28行目のコードは簡潔に訂正致しました。

--- c032.php
+++ c032.php
@@ -3,35 +3,28 @@
     // Let's チャレンジ！！
     $totalMerchandise = trim(fgets(STDIN));
     for($i = 0; $i < $totalMerchandise; $i++) {
-        $datas = explode(" ", trim(fgets(STDIN)));
-        // var_dump($datas[1]);
-        // die;
-        if ($datas[0] == 0) {
-            $foodPrice[] = $datas[1];
+        [$kindsNumber, $price] = explode(" ", trim(fgets(STDIN)));
+        if ($kindsNumber == 0) {
+            $foodPrice[] = $price;
         }
-        if ($datas[0] == 1) {
-            $bookPrice[] = $datas[1];
+        if ($kindsNumber == 1) {
+            $bookPrice[] = $price;
         }
-        if ($datas[0] == 2) {
-            $clothingPrice[] = $datas[1];
+        if ($kindsNumber == 2) {
+            $clothingPrice[] = $price;
         }
-        if ($datas[0] == 3) {
-            $otherPrice[] = $datas[1];
+        if ($kindsNumber == 3) {
+            $otherPrice[] = $price;
         }
     }
-    // var_dump($foodPrice);
     $totalFoodPrice = array_sum($foodPrice);
     $totalBookPrice = array_sum($bookPrice);
     $totalClothingPrice = array_sum($clothingPrice);
     $totalOtherPrice = array_sum($otherPrice);
-    $runcateFoodPrice = (floor(($totalFoodPrice)/100)*100);
-    $runcateBookPrice = (floor(($totalBookPrice)/100)*100);
-    $runcateClothingPrice = (floor(($totalClothingPrice)/100)*100);
-    $runcateOtherPrice = (floor(($totalOtherPrice)/100)*100);
-    $earnFoodPoint = $runcateFoodPrice * 0.05;
-    $earnBookPoint = $runcateBookPrice * 0.03;
-    $earnClothingPoint = $runcateClothingPrice * 0.02;
-    $earnOtherPoint = $runcateOtherPrice * 0.01;
-    $allPoint = $earnFoodPoint + $earnBookPoint + $earnClothingPoint + $earnOtherPoint;
-    echo $allPoint;
+    $runcateFoodPrice = (floor(($totalFoodPrice) / 100) * 100);
+    $runcateBookPrice = (floor(($totalBookPrice) / 100) * 100);
+    $runcateClothingPrice = (floor(($totalClothingPrice) / 100) * 100);
+    $runcateOtherPrice = (floor(($totalOtherPrice) / 100) * 100);
+    $totalPoint = $runcateFoodPrice * 0.05 + $runcateBookPrice * 0.03 + $runcateClothingPrice * 0.02 + $runcateOtherPrice * 0.01;
+    echo $totalPoint;
 ?>
