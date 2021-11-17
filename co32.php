e004157cefe3a6be34698a403587152b8c0666cd
Kotarooooo <wtrlndhr@gmail.com>
2021-11-17 11:48:00 +0900

    問題を一通り解き終えました。

--- co32.php
+++ co32.php
@@ -3,12 +3,30 @@
     // Let's チャレンジ！！
     $totalMerchandise = trim(fgets(STDIN));
     for($i = 0; $i < $totalMerchandise; $i++) {
-        $dates = explode(" ", trim(fgets(STDIN)));
-        if ($dates == 0) {
-            $foodPrice = array_sum($date[1]);
+        [$kindNumber, $price] = explode(" ", trim(fgets(STDIN)));
+        if ($kindNumber == 0) {
+            $foodPrice[] = $price;
         }
-     
+        if ($kindNumber == 1) {
+            $bookPrice[] = $price;
+        }
+        if ($kindNumber == 2) {
+            $clothingPrice[] = $price;
+        }
+        if ($kindNumber == 3) {
+            $otherPrice[] = $price;
+        }
     }
-            
+    $totalFoodPrice = array_sum($foodPrice);
+    $totalBookPrice = array_sum($bookPrice);
+    $totalClothingPrice = array_sum($clothingPrice);
+    $totalOtherPrice = array_sum($otherPrice);
+    $getFoodPoint = (floor(($totalFoodPrice) / 100) * 100 * 0.05);
+    $getBookPoint = (floor(($totalBookPrice) / 100) * 100 * 0.03);
+    $getClothingPoint = (floor(($totalClothingPrice) / 100) * 100 * 0.02);
+    $getOtherPoint = (floor(($totalOtherPrice) / 100) * 100 * 0.01);
+    $totalPoint = $getFoodPoint + $getBookPoint + $getClothingPoint + $getOtherPoint;
+    echo $totalPoint;
     
+
 ?>
